44df0e9cf2e52ddab6cb0e4228a4aacb22f20990
Vitor Hugo Pereira <78743212+VitorHugoPereiraa@users.noreply.github.com>
2021-05-22 22:23:18 -0300

    start register page

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
+import React from "react";
 
 // import { Container } from './styles';
 
 function Home() {
-<<<<<<< HEAD
   return <div />;
-=======
-  return (
-    <div>
-      <p>É O MAGRAS</p>
-    </div>
-  );
->>>>>>> f5fd652de05a03d2ff67e8cd40922a2a0dc0b3d1
 }
 
 export default Home;
